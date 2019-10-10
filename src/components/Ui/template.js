5ad8d443a36ad56a7bc6a392fae55084601483c7
sethukumar0001 <boylooksrock@gmail.com>
2019-10-10 22:49:51 +0530

    ui screen

---
+++ src/components/Ui/template.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './template.css'
+import Search from '../search/search';
+
+
+class Template extends Component {
+
+    render() {
+
+        return (
+            <div className="box">
+            <Search />
+            </div>
+            
+        )
+    } 
+
+}
+
+export default Template;
---
+++ src/components/search/search.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+
+    render() {
+
+        return(
+            <div> 
+            <form  >
+                <div>
+            <input           
+             type="text"
+              placeholder="Search.." 
+               />
+
+            </div>
+            </form>
+          </div>
+        )
+
+    }
+
+}
+export default Search
